9386342e8482af0d971720747b7eb48fa737833b
maksimgerasimenko <maksimgerasimenkoqa@gmail.com>
2016-10-25 01:19:02 +0300

    Revert "подключил Selenium 2.53 и импортировал"
    This reverts commit 5259e24b2575832b4a1359e63420bf39d2e50594.

--- sandbox/src/main/java/ru/stqa/pft/mine/CalculationOfDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/mine/CalculationOfDistance.java
@@ -7,6 +7,7 @@ public class CalculationOfDistance {
   public static void main(String[] args) {
 
     Point p1 = new Point(2, 3);
+
     Point p2 = new Point(4, 5);
 
     System.out.println("Расстояние между двумя точками на плоскости = " + p1.distance(p2));
