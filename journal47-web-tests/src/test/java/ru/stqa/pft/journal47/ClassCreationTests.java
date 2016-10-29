57e4727c803734a708f05bccd915cfd15e1a6c56
maksimgerasimenko <maksimgerasimenkoqa@gmail.com>
2016-10-30 00:49:24 +0300

    Отрефакторили тесты. В каждого отдельного теста есть свой вспомогательный метод

--- journal47-web-tests/src/test/java/ru/stqa/pft/journal47/ClassCreationTests.java
+++ journal47-web-tests/src/test/java/ru/stqa/pft/journal47/ClassCreationTests.java
@@ -16,26 +16,46 @@ public class ClassCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("https://journal47test.herokuapp.com/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
         wd.findElement(By.id("user_username")).click();
         wd.findElement(By.id("user_username")).clear();
-        wd.findElement(By.id("user_username")).sendKeys("admin");
+        wd.findElement(By.id("user_username")).sendKeys(username);
         wd.findElement(By.id("user_password")).click();
         wd.findElement(By.id("user_password")).clear();
-        wd.findElement(By.id("user_password")).sendKeys("secret");
-        wd.findElement(By.name("commit")).click();
+        wd.findElement(By.id("user_password")).sendKeys(password);
+        commitClassCreation();
     }
-    
+
     @Test
     public void testClassCreation() {
-        wd.findElement(By.linkText("Классы")).click();
-        wd.findElement(By.linkText("Добавить класс")).click();
+        goToClassPage();
+        initClassCreation();
+        fillClassForm(new ClassData("8-B"));
+        commitClassCreation();
+        goToClassPage();
+    }
+
+    private void commitClassCreation() {
+        wd.findElement(By.name("commit")).click();
+    }
+
+    private void fillClassForm(ClassData classData) {
         wd.findElement(By.id("form_name")).click();
         wd.findElement(By.id("form_name")).clear();
-        wd.findElement(By.id("form_name")).sendKeys("8-B");
-        wd.findElement(By.name("commit")).click();
+        wd.findElement(By.id("form_name")).sendKeys(classData.getNameForClass());
+    }
+
+    private void initClassCreation() {
+        wd.findElement(By.linkText("Добавить класс")).click();
+    }
+
+    private void goToClassPage() {
         wd.findElement(By.linkText("Классы")).click();
     }
-    
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
