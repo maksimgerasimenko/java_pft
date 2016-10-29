f4853f3e231698bff4741fce906fdd81e380cdc8
maksimgerasimenko <maksimgerasimenkoqa@gmail.com>
2016-10-30 00:46:50 +0300

    создан Parameter Object для данных про класс (имя класса)

---
+++ journal47-web-tests/src/test/java/ru/stqa/pft/journal47/ClassData.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.journal47;
+
+public class ClassData {
+  private final String nameForClass;
+
+  public ClassData(String nameForClass) {
+    this.nameForClass = nameForClass;
+  }
+
+  public String getNameForClass() {
+    return nameForClass;
+  }
+}
