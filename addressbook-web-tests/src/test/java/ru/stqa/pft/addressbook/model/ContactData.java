2f6d7c276dcbfe338fcbaf1ea910e84eb3a9ef6a
maksimgerasimenko <maksimgerasimenkoqa@gmail.com>
2016-11-01 14:11:34 +0200

    СОздали переменную group типа String для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -5,12 +5,14 @@ public class ContactData {
   private final String lastname;
   private final String phone;
   private final String mail;
+  private String group;
 
-  public ContactData(String name, String lastname, String phone, String mail) {
+  public ContactData(String name, String lastname, String phone, String mail, String group) {
     this.name = name;
     this.lastname = lastname;
     this.phone = phone;
     this.mail = mail;
+    this.group = group;
   }
 
   public String getName() {
@@ -27,5 +29,9 @@ public class ContactData {
 
   public String getMail() {
     return mail;
+  }
+
+  public String getGroup() {
+    return group;
   }
 }
